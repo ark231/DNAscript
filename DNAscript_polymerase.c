9b68f1c9a3daa68c01d7e4912eb1adc7ae4e37f9
ark231 <arklp20909@gmail.com>
2020-02-02 22:50:07 +0900

    add error add ext

--- DNAscript_polymerase.c
+++ DNAscript_polymerase.c
@@ -17,21 +17,27 @@
 #define NUM_INFILE 1
 #endif
 
+void error(char*);
 int main(int argc , char *argv[]){
+
   FILE *input=NULL,*output=NULL;
   char *outname=NULL;
   int inleng=strlen(argv[NUM_INFILE])+1;
   outname=(char*)malloc(inleng);
+  if(outname==NULL){
+    error("error: cannot malloc");
+  }
   strcpy(outname,argv[NUM_INFILE]);
-  strtok(outname,".");
+  outname=strtok(outname,".");
+  strtok(NULL,".");
 
   input=fopen(argv[NUM_INFILE],"r");
-  output=fopen();
+  output=fopen(outname,w);
 
 
 }
 
-int error(char *errmsg){
+void error(const char *errmsg){
   printf("%s\n",errmsg);
   exit(EXIT_FAILURE);
 }
