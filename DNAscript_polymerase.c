391b65c8d2d199eb18fac4182979471b96db9f46
ark231 <arklp20909@gmail.com>
2020-02-13 12:53:41 +0900

    add ERR_macro fix bug .pmrna free out_name, fclose

--- DNAscript_polymerase.c
+++ DNAscript_polymerase.c
@@ -10,6 +10,16 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define ERR_MALLOC "error: cannot malloc\n"
+#define ERR_NOTTRUEEXT "error: This isn't DNAscript source file \n extention must be \"DNA\"or\"dna\"\n"
+#define ERR_DAMEGEDDNA "error: This DNA is damaged\nhint: DNAscript source must NOT contain space/tab/newline\n"
+/*
+#define ERR_MALLOC "error: cannot malloc\n"
+#define ERR_MALLOC "error: cannot malloc\n"
+#define ERR_MALLOC "error: cannot malloc\n"
+#define ERR_MALLOC "error: cannot malloc\n"
+*/
+
 #define FOR_LINUX
 //#define FOR_WINDOWS
 //#define FOR_MAC
@@ -33,17 +43,18 @@ int main(int argc , char *argv[]){
   int inleng=strlen(argv[NUM_INFILE])+1;
   out_name=(char*)malloc(inleng);
   if(out_name==NULL){
-    error("error: cannot malloc\n");
+    error(ERR_MALLOC);
   }
   strcpy(out_name,argv[NUM_INFILE]);
   out_name=strtok(out_name,".");
   out_ext=strtok(NULL,".");
   if(strcmp(out_ext,"dna")!=0 && strcmp(out_ext,"DNA")!=0){//拡張子判定
-    error("error: This isn't DNAscript source file \n extention must be \"DNA\"or\"dna\"\n");
+    error(ERR_NOTTRUEEXT);
   }
-  sprintf(out_name,".pmrna");// pre-messengerRNA 伝令RNA前駆体
+  sprintf(out_name,"%s.pmrna",out_name);// pre-messengerRNA 伝令RNA前駆体
   input=fopen(argv[NUM_INFILE],"r");
   output=fopen(out_name,"w");
+  free(out_name);
   while((inchartmp=fgetc(input))!=EOF){
     switch(inchartmp){
       case 'A':
@@ -62,13 +73,19 @@ int main(int argc , char *argv[]){
       case 'g':
         fprintf(output,"C");
         break;
+      case ' ':
+      case '\t':
+      case '\n':
+        error(ERR_DAMEGEDDNA);
+        break;
       default:
         fprintf(output,"%c",inchartmp);
         break;
     }
   }
-
-
+  fclose(input);
+  fclose(output);
+  exit(EXIT_SUCCESS);
 }
 
 void error(const char *errmsg){
